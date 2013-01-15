95687ea4299bef411b5adfb3da9dad218f5c31c3
Andrew Smith <me@andrewmichaelsmith.com>
2013-01-15 07:05:13 -0800

    Adding initial setup.bash

---
+++ setup.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#add dionaea repo
+sudo add-apt-repository ppa:honeynet/nightly
+sudo apt-get update
+sudo apt-get install dionaea
+
+#make directories
+sudo mkdir -p /var/dionaea/wwwroot
+sudo mkdir -p /var/dionaea/binaries
+sudo mkdir -p /var/dionaea/log
+sudo chown -R nobody:nogroup /var/dionaea/
+
+#edit config
+sudo mv /etc/dionaea/dionaea.conf.dist /etc/dionaea/dionaea.conf
+sudo sed -i 's/var\/dionaea\///g' /etc/dionaea/dionaea.conf
+sudo sed -i 's/log\//\/var\/dionaea\/log\//g' /etc/dionaea/dionaea.conf
+sudo dionaea -c /etc/dionaea/dionaea.conf -w /var/dionaea -u nobody -g nogroup -D
