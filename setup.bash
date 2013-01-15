90087c96e695001a04ffb7e53518cc4480f82d0e
Andrew Smith <me@andrewmichaelsmith.com>
2013-01-15 15:25:08 +0000

    adding initial kippo setup

--- setup.bash
+++ setup.bash
@@ -3,6 +3,8 @@
 # dependency for add-apt-repository
 sudo apt-get install -y python-software-properties
 
+## install dionaea ##
+
 #add dionaea repo
 sudo add-apt-repository -y ppa:honeynet/nightly
 sudo apt-get update
@@ -18,4 +20,26 @@ sudo chown -R nobody:nogroup /var/dionaea/
 sudo mv /etc/dionaea/dionaea.conf.dist /etc/dionaea/dionaea.conf
 sudo sed -i 's/var\/dionaea\///g' /etc/dionaea/dionaea.conf
 sudo sed -i 's/log\//\/var\/dionaea\/log\//g' /etc/dionaea/dionaea.conf
+
+#launch dionaea
 sudo dionaea -c /etc/dionaea/dionaea.conf -w /var/dionaea -u nobody -g nogroup -D
+
+## install kippo - we want the latest so we have to grab the source ##
+
+#kippo dependencies
+sudo apt-get install -y subversion python-dev openssl python-openssl python-pyasn1 python-twisted iptables
+
+#install kippo to /opt/kippo
+sudo mkdir /opt/kippo/
+sudo svn checkout http://kippo.googlecode.com/svn/trunk/ /opt/kippo/
+sudo mv /opt/kippo/kippo.cfg.dist /opt/kippo/kippo.cfg
+
+#add kippo user that can't login
+sudo useradd -r -s /bin/false kippo
+
+#set up permissions
+sudo chown -R kippo:kippo /opt/kippo/
+
+sudo -u kippo sh start.sh
+
+sudo iptables -t nat -A PREROUTING -i IN_IFACE -p tcp --dport 22 -j REDIRECT --to-port 2222
