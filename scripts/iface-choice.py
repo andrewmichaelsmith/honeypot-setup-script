86e25c95a477bbef394c89d3ca2a224ae8345ee4
Andrew Smith <me@andrewmichaelsmith.com>
2013-01-17 13:47:54 +0000

    #7 initial commit. very much a wip.

--- scripts/iface-choice.py
+++ scripts/iface-choice.py
@@ -1,1 +1,25 @@
-# todo: implement python script to allow user to choose network interface
+#apt-get install python-pip
+#pip install netifaces
+import netifaces
+
+def select_iface(iface):
+   return "eth0"
+
+print "Please choose a network interface to run the honeypot on:\r\n"
+
+i = 0
+
+for ifaces in netifaces.interfaces():
+    print "\t[",i,"]",ifaces,"(",netifaces.ifaddresses(ifaces)[netifaces.AF_INET],")"
+    i = i+1
+
+print "\r\n"
+
+found = []
+while(not found):
+    found=select_iface(input('Chosen interface: '))
+
+
+print found
+
+
