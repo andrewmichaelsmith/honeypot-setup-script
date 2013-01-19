a315d3f1eb6a2263b279279e712cebe931c76d9e
Andrew Smith <me@andrewmichaelsmith.com>
2013-01-19 10:57:14 +0000

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -3,7 +3,7 @@
 #user iface choice
 sudo apt-get -y install python-pip gcc python-dev
 sudo pip install netifaces
-wget https://raw.github.com/andrewmichaelsmith/honeypot-setup-script/master/scripts/iface-choice.py -O /tmp/iface-choice.py
+sudo wget https://raw.github.com/andrewmichaelsmith/honeypot-setup-script/master/scripts/iface-choice.py -O /tmp/iface-choice.py
 python /tmp/iface-choice.py
 iface=$(<~/.honey_iface)
 
