5a14c9a6ebd1fd4350981d98bf5b293a440d4fc0
Michael Boman <michael@michaelboman.org>
2013-11-14 09:15:42 +0100

    Code cleanup Fail gracefully if the interface found not working properly (venet0 in my OpenVZ instance was detected but no such interface exists/if configured)

--- scripts/iface-choice.py
+++ scripts/iface-choice.py
@@ -10,21 +10,23 @@ def select_iface(iface):
             raise IndexError
         return netifaces.interfaces()[iface]
     except IndexError:
-        print "Number provided was too big or small"
+        print("Number provided was too big or small")
         return []
     except ValueError:
-        print "Please enter an interface number"
+        print("Please enter an interface number")
         return []
 
 
-print "Please choose a network interface to run the honeypot on:\r\n"
+print("Please choose a network interface to run the honeypot on:\r\n")
 
-i = 0
-for ifaces in netifaces.interfaces():
-    print "\t[",i,"]",ifaces,"(",netifaces.ifaddresses(ifaces)[netifaces.AF_INET],")"
-    i = i+1
+for i, ifaces in enumerate(netifaces.interfaces()):
+    try:
+        iface_text = "\t[%d] %s (%s)" % (i,ifaces,netifaces.ifaddresses(ifaces)[netifaces.AF_INET])
+        print(iface_text)
+    except Exception:
+        pass
 
-print "\r\n"
+print("\r\n")
 
 found = []
 while(not found):
@@ -32,6 +34,4 @@ while(not found):
 
 f = open(os.path.expanduser('~/.honey_iface'), 'w')
 f.write(found)
-
-
 
