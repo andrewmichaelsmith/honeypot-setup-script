de1f2d2a47f802ac631769c353a88c8037e57b48
Andrew Smith <me@andrewmichaelsmith.com>
2014-11-19 20:29:03 +0000

    let python script take command line arguments for automation

--- scripts/iface-choice.py
+++ scripts/iface-choice.py
@@ -1,14 +1,15 @@
-#apt-get install python-pip
-#pip install netifaces
 import netifaces
-import os 
+import os
+import sys
 
 def select_iface(iface):
     try:
         iface = int(iface)
         if(iface < 0):
             raise IndexError
+
         return netifaces.interfaces()[iface]
+
     except IndexError:
         print("Number provided was too big or small")
         return []
@@ -17,21 +18,41 @@ def select_iface(iface):
         return []
 
 
-print("Please choose a network interface to run the honeypot on:\r\n")
+def get_args():
+    if len(sys.argv) > 1:
+        return netifaces.interfaces()[int(sys.argv[1])]
 
-for i, ifaces in enumerate(netifaces.interfaces()):
-    try:
-        iface_text = "\t[%d] %s (%s)" % (i,ifaces,netifaces.ifaddresses(ifaces)[netifaces.AF_INET])
-        print(iface_text)
-    except Exception:
-        pass
+def get_user_input():
 
-print("\r\n")
+    print("Please choose a network interface to run the honeypot on:\r\n")
 
-found = []
-while(not found):
-    found=select_iface(raw_input('Chosen interface: '))
+    for i, ifaces in enumerate(netifaces.interfaces()):
+        try:
+            iface_text = "\t[%d] %s (%s)" % (i,ifaces,netifaces.ifaddresses(ifaces)[netifaces.AF_INET])
+            print(iface_text)
+        except Exception:
+            pass
 
-f = open(os.path.expanduser('~/.honey_iface'), 'w')
-f.write(found)
+    print("\r\n")
+
+    found = []
+    while(not found):
+        found=select_iface(raw_input('Chosen interface: '))
+    return found
+
+def run():
+
+
+    args = get_args()
+
+    found = get_args()  if get_args() else get_user_input()
+
+    print found
+
+    f = open(os.path.expanduser('~/.honey_iface'), 'w')
+    f.write(found)
+
+if __name__ == "__main__":
+    run()
+
 
